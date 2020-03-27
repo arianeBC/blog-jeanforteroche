6470ae89d9c4e19fa80e1b10309d58db3076d3be
Ariane DESVALS <arianebrosseaucote@gmail.com>
2020-03-27 15:27:37 +1100

    arrow pagination added

--- episodes.php
+++ episodes.php
@@ -55,9 +55,22 @@ include( ROOT_PATH . "/models/post.php");
                // divided by limit of posts by pages & ceil for whole nb 
                $totalPages = ceil($totalRecords/6);
                $pageLink = "<ul class='pagination justify-content-center'>";
+
+               $page = $_GET['page'];
+               if($page > 1) {
+                  // $pageLink .= "<a class='page-link' href='episodes.php?page=1'>First</a>";
+                  $pageLink .= "<a class='arrow' href='episodes.php?page=".($page-1)."'><i class='fas fa-arrow-left'></i></a>";
+               }
+
                for($i=1; $i<=$totalPages; $i++) {
                   $pageLink .= "<a class='page-link' href='episodes.php?page=" .$i. "'>" .$i. "</a>";
                }
+
+               if($page < $totalPages) {
+                  $pageLink .= "<a class='arrow' href='episodes.php?page=".($page+1)."'><i class='fas fa-arrow-right'></i></a>";
+                  // $pageLink .= "<a class='page-link' href='episodes.php?page=".$totalPages."'>Last</a>";
+               }
+
                echo $pageLink."</ul>";
                ?>
                </div>
