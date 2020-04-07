07ac5b2819f32639edd9929a47ade63561e16926
Ariane DESVALS <arianebrosseaucote@gmail.com>
2020-04-07 11:59:09 +1100

    db connexion with PDO

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,11 @@
 <?php 
+   // DB Params
+   define("DB_HOST", "localhost");
+   define("DB_USER", "root");
+   define("DB_PASS", "qwerty");
+   define("DB_NAME", "jeanforteroche");
+
+
    // App Root
    define( "APPROOT", dirname(dirname(__FILE__)) );
    // URL Root
